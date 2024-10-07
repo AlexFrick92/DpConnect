809c3fedd4430f261c4ee7bf87ebeffb7076ccbd
AKFrick <167059874+AlexFrick92@users.noreply.github.com>
2024-10-07 15:24:33 +0500

    example of subscribtion to bool node

---
+++ DpConnect.SimpleSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+using Promatis.Core.Logging;
+
+using DpConnect.Configuration;
+using DpConnect.Provider.OpcUa;
+
+
+
+namespace DpConnect.SimpleSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            ConsoleLogger logger = new ConsoleLogger();
+
+            DpFluentBuilder dataPointConfigurator = new DpFluentBuilder()
+                .SetLogger(logger)
+                .AddConfiguration($"{currentDirectory}/DpConfig.xml")                
+                .SetProviders(new Type[] { typeof(OpcUaProvider) })
+                .SetProcessors(new Type[] { typeof(ReadNodeProcessor)})                
+                .Build();
+
+           // dataPointConfigurator.StartProviders();
+
+
+            while (true)
+            {
+                var inputString = Console.ReadLine();
+
+                switch (inputString)
+                {
+
+                    case "exit": return;                        
+                }
+            }
+        }
+    }
+}
---
+++ DpConnect.SimpleSample/ReadNodeProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DpConnect.Interface;
+
+namespace DpConnect.SimpleSample
+{
+    internal class ReadNodeProcessor : DpProcessor
+    {        
+        public ReadNodeProcessor()
+        {            
+            DpInitialized += ReadNodeProcessor_DpInitialized;
+        }
+
+        private void ReadNodeProcessor_DpInitialized(object sender, System.EventArgs e)
+        {
+            BoolNode.ValueUpdated += BoolNode_ValueUpdated;
+        }
+
+        private void BoolNode_ValueUpdated(object sender, bool e)
+        {
+            Console.WriteLine("Считано новое значение по подписке: " + e);
+        }
+
+        public IDpValue<bool> BoolNode { get; set; }
+    }
+}
