bc8d713da575ccc081712eb8c3b14c41bff6ba96
AKFrick <167059874+AlexFrick92@users.noreply.github.com>
2024-11-15 14:23:27 +0500

    проект с примером считывания и записи простых точек

---
+++ DpConnect.Example.PrimitiveValues/ConsoleReader.cs
@@ -0,0 +1,59 @@
+﻿using Promatis.Core.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace DpConnect.Example.PrimitiveValues
+{
+    internal class ConsoleReader : IConsoleReader
+    {
+        ILogger logger;
+
+        public IDpValue<bool> BoolDp { get; set; }
+
+        public IDpValue<float> FloatDp { get; set; }
+
+        public void DpBound()
+        {
+            BoolDp.ValueUpdated += (s, v) => logger.Info("Значение булевой точки: {0}", v);
+            FloatDp.ValueUpdated += (s, v) => logger.Info("Значение вещественной точки: {0}", v);
+
+            Task.Run(async ()=>                
+                {
+                    while(true)
+                    {
+                        if (BoolDp.IsConnected)
+                        {
+                            BoolDp.Value = !BoolDp.Value;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Нет соединения");
+                        }
+                        await Task.Delay(1000);
+                    }
+                });
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        FloatDp.Value = FloatDp.Value + 0.6f;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Исключение при попытке записи:" + ex.Message);
+                    }
+                    await Task.Delay(500);
+                }
+            });
+
+        }
+
+        public ConsoleReader(ILogger logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
---
+++ DpConnect.Example.PrimitiveValues/Program.cs
@@ -0,0 +1,53 @@
+﻿using DpConnect.Configuration.Xml;
+using DpConnect.OpcUa;
+
+using Promatis.Core;
+using Promatis.Core.Logging;
+using Promatis.IoC.DryIoc;
+using System;
+using System.Threading.Tasks;
+
+
+
+
+namespace DpConnect.Example.PrimitiveValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IIoCContainer container = new DryIocContainer();
+
+
+            container.RegisterInstance(container);
+
+            container.RegisterInstance(new ConsoleLogger() as ILogger);
+
+            container.Register<IOpcUaConnection, OpcUaConnection>();
+            container.Register<IDpConnectionManager, ContainerizedConnectionManager>();
+
+
+            container.Register<IConsoleReader, ConsoleReader>();
+            container.Register<IDpWorkerManager, ContainerizedWorkerManager>();
+
+            container.Register<IDpBuilder, DpXmlBuilder>();
+
+
+
+
+
+            IDpBuilder builder = container.Resolve<IDpBuilder>();
+            builder.Build();
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                builder.ConnectionManager.OpenConnections();
+            });
+
+
+            Console.ReadLine();
+        }
+    }
+}
