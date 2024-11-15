c4ac5cc713a7e519a5b99bb82fa1f1fdefe38eb4
AKFrick <167059874+AlexFrick92@users.noreply.github.com>
2024-11-15 11:03:49 +0500

    добавил комметарии к классам IDpAction IDpValue IDpWorker

--- DpConnect/IDpValue.cs
+++ DpConnect/IDpValue.cs
@@ -1,13 +1,22 @@
-﻿
-
-using System;
+﻿using System;
 
 namespace DpConnect
 {
+    /// <summary>
+    /// Объявляется как открытое свойство IDpWorker. В это свойством будет присвоен объект, через который поступают данные
+    /// </summary>
+    /// <typeparam name="T"> Тип Т - примитивный тип, DateTime, string или класс с открытыми свойствами. Имя свойства должно совпадать с именем свойства в структуре OPC UA </typeparam>
     public interface IDpValue<T> where T : new()
     {
+        /// <summary>
+        /// Свойство, через которое можно получить актуальное значение
+        /// Через этой свойство так же записывается значение
+        /// </summary>
         T Value { get; set; }
 
+        /// <summary>
+        /// Возникает при обновлении Value из источника данных
+        /// </summary>
         event EventHandler<T> ValueUpdated;        
     }
 }
