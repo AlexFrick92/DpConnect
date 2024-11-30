d4b8f8d69c0d84154277a6801d6bf4883b96d7bb
AKFrick <ak.frick@promatis.ru>
2024-11-30 11:53:24 +0500

    create UI example project

---
+++ DpConnect.Example.TechParamApp/App.cs
@@ -0,0 +1,24 @@
+﻿using DpConnect.Example.TechParamApp.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpConnect.Example.TechParamApp
+{
+    internal class App : System.Windows.Application
+    {
+        public App()
+        {
+            
+        }
+        public new void Run()
+        {
+            MainView view = new MainView();
+            view.Show();
+
+            base.Run();
+        }
+    }
+}
